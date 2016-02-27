0a4e1ce5d682fc7c9d2f023cd42bf1ce42bd34e4
Utkarsh <arsh.utkarsh@gmail.com>
2016-02-27 17:02:33 +0530

    ER

---
+++ myapp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+import account.views
+
+import myapp.forms
+
+
+class SignupView(account.views.SignupView):
+
+   form_class = myapp.forms.SignupForm
+
+   def after_signup(self, form):
+       self.create_profile(form)
+       super(SignupView, self).after_signup(form)
+
+   def create_profile(self, form):
+       profile = self.created_user.userprofile  # replace with your reverse one-to-one profile attribute
+       profile.birthdate = form.cleaned_data["birthdate"]
+       profile.save()
